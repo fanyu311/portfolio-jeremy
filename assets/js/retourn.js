f7d9756e368d533c26dfdc035e20e72e35231f6b
fanyu311 <116265245+fanyu311@users.noreply.github.com>
2023-09-27 16:55:30 +0200

    feat:add js

--- assets/js/retourn.js
+++ assets/js/retourn.js
@@ -2,12 +2,14 @@ function goTop() {
 
     const btnFooter = document.querySelector('.ph-arrow-up')
 
-    btnFooter.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-          });
-    })
+    if (btnFooter) {
+        btnFooter.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+              });
+        })
+    }
 }
 goTop()
