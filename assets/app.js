6c6bf21621dc132d8e1dfe2a4a947f3cc2fc384e
fanyu311 <y311@live.cn>
2023-12-15 23:49:51 +0100

    feat: corige webpack

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 import './bootstrap.js';
-// 
+//
 
 /*
  * Welcome to your app's main JavaScript file!
@@ -9,6 +9,7 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+
 import './styles/app.scss';
 
 //import zoomImage
